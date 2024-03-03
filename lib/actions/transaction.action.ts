84fa05b50ebdc76f5690dde8a17324dea4b54a1c
Fireball.Dev <fireball.dev99@gmail.com>
2024-03-03 20:21:54 +0700

    fix transaction

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -16,7 +16,7 @@ export async function checkoutCredits(transaction: CheckoutTransactionParams) {
     line_items: [
       {
         price_data: {
-          currency: "thb",
+          currency: "usd",
           unit_amount: amount,
           product_data: {
             name: transaction.plan,
